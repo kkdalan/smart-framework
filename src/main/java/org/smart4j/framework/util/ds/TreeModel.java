af17265a94b9e7042d793ea6904e9387dc876da0
Alan Huang <alanhuang@inquartik.com>
2021-04-26 18:33:22 +0800

    add TreeModel & PrintUtils

---
+++ src/main/java/org/smart4j/framework/util/ds/TreeModel.java
@@ -0,0 +1,76 @@
+package org.smart4j.framework.util.ds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeModel<T> {
+
+  T value;
+  List<TreeModel<T>> children = new ArrayList<>();
+  TreeModel<T> parent;
+
+  public TreeModel(T value) {
+    this.value = value;
+  }
+  
+  public T getValue() {
+    return value;
+  }
+  
+  public void setValue(T value) {
+    this.value = value;
+  }
+
+  public void add(TreeModel<T> node) {
+    node.setParent(this);
+    children.add(node);
+  }
+
+  public void remove(TreeModel<T> node) {
+    if (hasChildren()) {
+      node.setParent(null);
+      children.remove(node);
+    }
+    throw new UnsupportedOperationException();
+  }
+
+  public TreeModel<T> getChild(int i) {
+    return children.get(i);
+  }
+
+  public List<TreeModel<T>> getChildren() {
+    return children;
+  }
+  
+  public int totalSize() {
+    int size = 1;
+    if(hasChildren()) {
+      for(TreeModel<T> child : children) {
+        size += child.totalSize();
+      }
+      return size;
+    }
+    return size;
+  }
+  
+  public int childrenSize() {
+    return children != null ? children.size() : 0;
+  }
+
+  public boolean hasChildren() {
+    return childrenSize() > 0;
+  }
+  
+  public void setParent(TreeModel<T> parent) {
+    this.parent = parent;
+  }
+
+  public TreeModel<T> getParent() {
+    return parent;
+  }
+  
+  public void printTree() {
+    System.out.println(TreeModelUtils.getTreeDiagram(this));
+  }
+
+}
---
+++ src/main/java/org/smart4j/framework/util/ds/TreeModelUtils.java
@@ -0,0 +1,83 @@
+package org.smart4j.framework.util.ds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeModelUtils {
+
+	public static <T> List<TreeModel<T>> flattenModel(TreeModel<T> root) {
+		List<TreeModel<T>> result = flatten(root, new ArrayList<TreeModel<T>>());
+		return result;
+	}
+
+	private static <T> List<TreeModel<T>> flatten(TreeModel<T> node, List<TreeModel<T>> flatList) {
+		if (node != null) {
+			flatList.add(node);
+			if (node.hasChildren()) {
+				List<TreeModel<T>> children = node.getChildren();
+				for (TreeModel<T> child : children) {
+					if (child.getChildren() != null) {
+						flatten(child, flatList);
+					}
+				}
+			}
+		}
+		return flatList;
+	}
+
+	public static <T> String getTreeDiagram(TreeModel<T> root) {
+		if (root.getValue() == null) {
+			return "";
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(root.getValue());
+		sb.append("\n");
+
+		String pointerRight = "└──";
+		String pointerLeft = (root.hasChildren()) ? "├──" : "└──";
+
+		List<TreeModel<T>> children = root.getChildren();
+		for (int i = 0; i < children.size(); i++) {
+			TreeModel<T> childNode = (TreeModel<T>) children.get(i);
+			if (i < children.size() - 1) {
+				traversePreOrder(sb, "", pointerLeft, childNode, root.hasChildren());
+			} else {
+				traversePreOrder(sb, "", pointerRight, childNode, false);
+			}
+		}
+		return sb.toString();
+	}
+
+	private static <T> void traversePreOrder(StringBuilder sb, String padding, String pointer, TreeModel<T> node,
+			boolean hasRightSibling) {
+		if (node.getValue() != null) {
+			sb.append(padding);
+			sb.append(pointer);
+			sb.append(node.getValue());
+			sb.append("\n");
+
+			StringBuilder paddingBuilder = new StringBuilder(padding);
+			if (hasRightSibling) {
+				paddingBuilder.append("│  ");
+			} else {
+				paddingBuilder.append("   ");
+			}
+
+			String paddingForAll = paddingBuilder.toString();
+			String pointerRight = "└──";
+			String pointerLeft = (node.hasChildren()) ? "├──" : "└──";
+
+			List<TreeModel<T>> children = node.getChildren();
+			for (int i = 0; i < children.size(); i++) {
+				TreeModel<T> childNode = (TreeModel<T>) children.get(i);
+				if (i < children.size() - 1) {
+					traversePreOrder(sb, paddingForAll, pointerLeft, childNode, node.hasChildren());
+				} else {
+					traversePreOrder(sb, paddingForAll, pointerRight, childNode, false);
+				}
+			}
+		}
+	}
+
+}
